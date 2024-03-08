0c958b2f3942fb15bbb654a0e55977733c50a5b1
stvjudge <istvan.biro03@gmail.com>
2024-03-08 22:05:44 +0100

    update

---
+++ .config/nvim/lua/utils/colorscheme.lua
@@ -0,0 +1,28 @@
+return {
+
+  -- download colorschemes
+  -- these are here for trying out different themes
+  {
+    -- "wittyjudge/gruvbox-material.nvim",
+    "sainnhe/gruvbox-material",
+    "sainnhe/sonokai",
+    "sainnhe/edge",
+    "AlexvZyl/nordic.nvim",
+    "catppuccin/nvim",
+    "folke/tokyonight.nvim",
+    "AlexvZyl/nordic.nvim",
+  },
+
+  -- set colorscheme
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      -- colorscheme = "gruvbox-material",
+      -- colorscheme = "edge",
+      -- colorscheme = "sonokai",
+      colorscheme = "nordic",
+      -- colorscheme = "catppuccin",
+      -- colorscheme = "tokyonight",
+    },
+  },
+}
