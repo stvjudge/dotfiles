490fc560bb1743de6b8bc6c4af0c6f3394d1e192
stvjudge <istvan.biro03@gmail.com>
2024-03-18 23:24:27 +0100

    upload nvim config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,1 +1,3 @@
-require("stv")
+require("ib.options")
+
+require("ib.lazy")
---
+++ .config/nvim/lua/ib/options.lua
@@ -0,0 +1,33 @@
+local opt = vim.opt
+local o = vim.o
+local g = vim.g
+
+g.mapleader = ' '
+g.maplocalleader = ' '
+
+opt.number = true
+opt.relativenumber = false
+
+opt.mouse = "a"
+
+opt.showmode = false
+
+opt.clipboard = "unnamedplus"
+
+opt.breakindent = true
+
+opt.ignorecase = true
+opt.smartcase = true
+
+opt.cursorline = true
+
+o.expandtab = true
+o.tabstop = 4
+o.shiftwidth = 4
+o.smartindent = true
+
+opt.listchars = {
+    space = "·",
+    eol = "↵",
+}
+opt.list = true
