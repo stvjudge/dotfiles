60e25fc59e82028f7f6f43f483bb45989697db4a
Istvan Biro <istvan.biro03@gmail.com>
2025-02-20 14:00:34 +0100

    feat(dot): added sonokai colorscheme

--- nvim/.config/nvim/lua/plugins/colorschemes.lua
+++ nvim/.config/nvim/lua/plugins/colorschemes.lua
@@ -1,34 +1,44 @@
 local g = vim.g
 
 return {
-    {
-        "projekt0n/github-nvim-theme",
-        name = "github-theme",
-        lazy = false,
-        priority = 1000,
-        opts = {},
-    },
-    {
-        "rjshkhr/shadow.nvim",
-        lazy = false,
-        priority = 1000,
-        config = function() end,
-    },
-    {
-        "folke/tokyonight.nvim",
-        priority = 1000,
-        lazy = false,
-        opts = {},
-    },
-    {
-        "sainnhe/gruvbox-material",
-        priority = 1000,
-        lazy = false,
+  {
+    "projekt0n/github-nvim-theme",
+    name = "github-theme",
+    lazy = false,
+    priority = 1000,
+    opts = {},
+  },
+  {
+    "rjshkhr/shadow.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function() end,
+  },
+  {
+    "folke/tokyonight.nvim",
+    priority = 1000,
+    lazy = false,
+    opts = {},
+  },
+  {
+    "sainnhe/sonokai",
+    priority = 1000,
+    lazy = false,
 
-        config = function()
-            g.gruvbox_matrial_enable_italic = true
-            g.gruvbox_material_background = "hard"
-            g.gruvbox_material_performance = true
-        end,
-    },
+    config = function()
+      g.sonokai_enable_italic = "1"
+      g.sonokai_style = "andromeda"
+    end,
+  },
+  {
+    "sainnhe/gruvbox-material",
+    priority = 1000,
+    lazy = false,
+
+    config = function()
+      g.gruvbox_matrial_enable_italic = true
+      g.gruvbox_material_background = "hard"
+      g.gruvbox_material_performance = true
+    end,
+  },
 }
