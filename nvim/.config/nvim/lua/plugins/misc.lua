9504a100f04556271dc1c0d2a7199d8057904f85
Istvan Biro <istvan.biro03@gmail.com>
2025-03-06 15:29:25 +0100

    feat(dot): many updates, added yazi

--- nvim/.config/nvim/lua/plugins/misc.lua
+++ nvim/.config/nvim/lua/plugins/misc.lua
@@ -1,24 +1,10 @@
 return {
     {
-        "neo451/feed.nvim",
-        cmd = "Feed",
-        opts = {
-            feeds = {
-                "https://archlinux.org/feeds/news/",
-                {
-                    "https://archlinux.org/feeds/news/",
-                    name = "Arch Linux News",
-                    tags = {},
-                },
-            },
-        },
-    },
-    {
         "m4xshen/hardtime.nvim",
         dependencies = { "MunifTanjim/nui.nvim" },
         opts = {
             disable_mouse = false,
-            restriction_mode = "hint",
+            -- restriction_mode = "hint",
         },
     },
 }
