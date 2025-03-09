efcd815ffb8d87eee59cedf2828954a6c289b419
Istvan Biro <istvan.biro03@gmail.com>
2025-03-09 22:30:48 +0100

    feat(dot): plugin updatess

--- nvim/.config/nvim/lua/plugins/colorschemes.lua
+++ nvim/.config/nvim/lua/plugins/colorschemes.lua
@@ -8,10 +8,15 @@ return {
 
         config = function()
             g.gruvbox_matrial_enable_italic = true
-            g.gruvbox_material_background = "medium"
+            g.gruvbox_material_background = "hard"
             g.gruvbox_material_performance = true
             g.gruvbox_material_transparent_background = 1
             vim.cmd.colorscheme("gruvbox-material")
         end,
+    },
+    {
+        "RRethy/base16-nvim",
+        lazy = false,
+        -- opts = {},
     },
 }
