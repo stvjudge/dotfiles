e7dad808f811a34e89e421945cbc25f70fb2b43f
Istvan Biro <istvan.biro03@gmail.com>
2025-03-11 17:00:39 +0100

    feat(dot): many updates to plugins, added sops pkg

--- nvim/.config/nvim/lua/plugins/colorschemes.lua
+++ nvim/.config/nvim/lua/plugins/colorschemes.lua
@@ -1,25 +1,47 @@
 local g = vim.g
-
 return {
     {
-        "sainnhe/gruvbox-material",
-        priority = 1000,
+        "f4z3r/gruvbox-material.nvim",
+        name = "gruvbox-material",
         lazy = false,
-
-        config = function()
-            g.gruvbox_matrial_enable_italic = true
-            g.gruvbox_material_background = "hard"
-            g.gruvbox_material_performance = true
-            g.gruvbox_material_transparent_background = 1
-            vim.cmd.colorscheme("gruvbox-material")
-        end,
+        priority = 1000,
+        opts = {
+            contrast = "hard",
+            background = {
+                transparent = true,
+            },
+        },
     },
+    -- {
+    --     "sainnhe/gruvbox-material",
+    --     priority = 1000,
+    --     lazy = false,
+    --
+    --     config = function()
+    --         g.gruvbox_matrial_enable_italic = true
+    --         g.gruvbox_material_background = "hard"
+    --         g.gruvbox_material_performance = true
+    --         g.gruvbox_material_transparent_background = 1
+    --         vim.cmd.colorscheme("gruvbox-material")
+    --     end,
+    -- },
     {
         "RRethy/base16-nvim",
         lazy = false,
         -- opts = {},
     },
-    "rose-pine/neovim",
-    name = "rose-pine",
-    lazy = false,
+    {
+        "rose-pine/neovim",
+        name = "rose-pine",
+        lazy = false,
+    },
+    {
+        "ellisonleao/gruvbox.nvim",
+        priority = 1000,
+        lazy = false,
+        opts = {
+            contrast = "hard",
+            transparent_mode = true,
+        },
+    },
 }
