42a8a48676f228cb9d6ef05ac8c9dac55b01e78c
Istvan Biro <istvan.biro03@gmail.com>
2025-03-20 17:55:01 +0100

    feat(dot): update

--- nvim/.config/nvim/lua/plugins/colorschemes.lua
+++ nvim/.config/nvim/lua/plugins/colorschemes.lua
@@ -3,15 +3,19 @@ return {
     {
         "projekt0n/github-nvim-theme",
         name = "github-theme",
-        lazy = false,    -- make sure we load this during startup if it is your main colorscheme
+        lazy = false, -- make sure we load this during startup if it is your main colorscheme
         priority = 1000, -- make sure to load this before all the other start plugins
         config = function()
-            require('github-theme').setup({
-
-            })
+            require("github-theme").setup({})
             vim.cmd.colorscheme("github_dark_default")
         end,
     },
+    -- {
+    --     "Skardyy/makurai-nvim",
+    --     config = function()
+    --         vim.cmd.colorscheme("makurai")
+    --     end,
+    -- },
     {
         "sainnhe/gruvbox-material",
         priority = 1000,
