37bcea72c6e37a129e274b653bea1614359878ff
Istvan Biro <istvan.biro03@gmail.com>
2025-04-07 14:48:27 +0200

    feat(dot): removed bufferline, added cloak, some plugin update

--- nativelsp/.config/nativelsp/lua/plugins/utils.lua
+++ nativelsp/.config/nativelsp/lua/plugins/utils.lua
@@ -1,12 +1,30 @@
 return {
+    -- Visualise whitespaces when text is selected
     {
         "mcauley-penney/visual-whitespace.nvim",
         config = true,
         -- keys = { 'v', 'V', '<C-v>' }, -- optionally, lazy load on visual mode keys
     },
+    -- Helm helper for vim
     {
         "towolf/vim-helm",
         lazy = true,
         opts = {},
+    },
+    -- Hide any sensitive data in files specified in the patterns
+    {
+        "laytan/cloak.nvim",
+        name = "Cloak",
+        opts = {
+            patterns = {
+                {
+                    file_pattern = {
+                        ".env*",
+                        "credentials",
+                    },
+                    cloak_pattern = { "=.+", ":.+" },
+                },
+            },
+        },
     },
 }
